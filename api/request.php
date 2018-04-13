0a3e784a9d329432686cc7c3e9dba9b05e34e013
Moses Moore <mozai@users.noreply.github.com>
2018-04-13 18:09:06 +0000

    Request UI is now available

--- api/request.php
+++ api/request.php
@@ -1,32 +1,52 @@
 <?php
     header('Content-Type: application/json');
-    require_once('config.php');
+    require_once('../config.php');
     $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME .';charset=utf8', DB_USER , DB_PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-    $answer = array('success' => 0, 'error' => 'null');
-    $client_ip = $_SERVER['REMOTE_ADDR'];
-    $req_count = $db->prepare("SELECT * FROM requests WHERE reqsrc = :remoteip");
-    $req_count->execute(array(':remoteip' => $client_ip));
-    if ($req_count->rowCount() >= 3) {
-        $answer = array('success'=> 0, 'error' => 'Too many pending requests');
+    function submit_request($reqid, $reqname, $reqsrc) {
+        global $db;
+        $cur = $db->prepare("SELECT id, title FROM library WHERE id = :sid and requestable = 1");
+        $cur->execute(array(':sid' => $reqid));
+        if ($cur->rowCount() < 1)
+            return array('success' => 0, 'message' => 'Not a requestable song' . $reqid);
+        $song = $cur->fetch(PDO::FETCH_ASSOC);
+        $cur = $db->prepare("INSERT INTO requests (reqid, reqname, reqsrc) VALUES (:reqid, :reqname, :reqsrc)");
+        $status = $cur->execute(array(':reqid' => $reqid, ':reqname' => $reqname, ':reqsrc' => $reqsrc));
+        if ($status)
+            return array('success' => 1, 'message' => 'song request queued', 'reqid' => $reqid, 'title' => $song['title']);
+        else
+            return array('success' => 0, 'message' => 'db insert failed');
     }
-    else if (isset($_POST['song']) && isset($_POST['name'])) {
-        $is_exists = $db->prepare("SELECT id, title FROM library WHERE id = :sid");
-        $is_exists->execute(array(':sid' => $_POST['song']));
-        if ($is_exists->rowCount() < 1) {
-            $answer = array('success'=> 0, 'error' => 'no such songid '. $_POST['song']); 
-        }
-        else {
-            $song = $is_exists->fetch(PDO::FETCH_ASSOC);
-            $req = $db->prepare("INSERT INTO requests (reqid, reqname, reqsrc) VALUES (:reqid, :reqname:, :reqsrc)");
-            $req->execute(array(':reqid' => $_POST['song'], ':reqname' => $_POST['name'], ':reqsrc' => $_SERVER['REMOTE_ADDR']));
-            $answer = array('success' => 1, 'message' => "\"" . $song['title'] . "\" queued");
-        }
+
+    $cur = $db->prepare("SELECT sum(1) from requests where reqsrc = :reqsrc");
+    $cur->execute(array(':reqsrc' => $_SERVER['REMOTE_ADDR']));
+    $your_queue_length = (int)$cur->fetch(PDO::FETCH_COLUMN, 0);
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $cur = $db->prepare("SELECT songid from recent where time > date_sub(now(), interval 1 hour) UNION select reqid as songid from requests;");
+        $cur->execute();
+        $frozen_songids = $cur->fetchAll(PDO::FETCH_COLUMN, 0);
+        sort($frozen_songids);
+        $cur = $db->prepare("SELECT sum(1) from requests");
+        $cur->execute();
+        $queue_length = (int)$cur->fetch(PDO::FETCH_COLUMN, 0);
+        $answer = array('req_count' => $queue_length, 'your_req_count' => $your_queue_length, 'on_cooldown' => $frozen_songids);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (! (isset($_POST['song']) && isset($_POST['name'])))
+            $answer = array('success' => 0, 'message' => 'missing input');
+        else if ($your_queue_length > REQUEST_LIMIT)
+            $answer = array('success' => 0, 'message' => 'Too many pending requests');
+        else if ($queue_length > (REQUEST_LIMIT * 5))
+            $answer = array('success' => 0, 'message' => 'Too many pending requests');
+        else if (in_array($_POST['song'], $frozen_songids))
+            $answer = array('success' => 0, 'message' => 'Requested song is on cooldown');
+        else
+            $answer = submit_request($_POST['song'], $_POST['name'], $_SERVER['REMOTE_ADDR']);
     }
     else {
-        $answer = array('success' => 0, 'error' => 'missing input');
-    } 
+        $answer = array('success' => 0, 'message' => 'Unknown request type');
+    }
     echo json_encode($answer);
 ?>
---
+++ request.php
@@ -0,0 +1,141 @@
+<?php
+    require_once('config.php');
+    # TODO: mouseover of halflings-picture to get a pop-in of the album art
+    $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME .';charset=utf8', DB_USER , DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $songs = $db->prepare("SELECT * FROM library WHERE autoplay='1' OR requestable='1' ORDER BY album");
+    $songs->execute();
+    
+    $client_ip = $_SERVER['REMOTE_ADDR'];
+    $req_count = $db->prepare("SELECT * FROM requests WHERE reqsrc='" . $client_ip ."'");
+    $req_count->execute();
+    $disabled = ($req_count->rowCount() >= 3)
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Skaianet Radio (BETA)</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/vendor/bootstrap.min.css">
+        
+        <!-- Material Design CSS -->
+        <link rel="stylesheet" href="css/vendor/material.min.css">
+        <link rel="stylesheet" href="css/vendor/ripples.min.css">
+        <link rel="stylesheet" href="css/vendor/snackbar.min.css">
+
+        <!-- Roboto Font -->
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:500,300,700,400">
+        
+        <style>
+            body {
+                padding-top: 20px;
+                padding-bottom: 20px;
+                font-family: Roboto;
+            }
+            
+            th {
+                cursor: pointer;
+            }
+            
+            .link-material-blue {
+                color: #2196f3;
+            }
+            
+            .link-material-blue:hover {
+                color: #2196f3;
+            }
+            table#songlist tbody tr td {
+                vertical-align: middle;
+            }
+        </style>
+        
+        <!-- Mondernizer JS and Respond JS -->
+        <script src="js/vendor/modernizr.min.js"></script>
+        <script src="js/vendor/respond.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="panel panel-material-red" id="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title" style="font-size:18pt;"><span class="mdi-av-my-library-music"></span> Request</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="form-group has-warning col-sm-4">
+                        <label class="control-label" for="inputLarge">Nickname</label>
+                        <input class="form-control" type="text" id="request-name" placeholder="Anonymous" />
+                    </div> 
+                    <div class="form-group has-warning col-sm-4">
+                        <label class="control-label" for="inputLarge">&#x1f50e;</label>
+                        <input class="form-control " type="text" id="search-text" placeholder="search" />
+                    </div> 
+                    <div class=form-group" id="queue_status">
+                        Requests Queue: 0<br>Your Requests: 0
+                    </div>   
+                    <table id="songlist" class="table table-striped table-hover sortable">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Album</th>
+                                <th>Artist</th>
+                                <th>Request</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            foreach ($songs as $song) {
+                                echo "<tr>";
+                                echo "<td>" . $song["title"] . "</td>";
+                                echo "<td>" . $song["album"] . "</td>";
+                                echo "<td>" . $song["artist"] . "</td>";
+                                echo "<td><button type=submit name=request-button-" . $song["id"] . " ";
+                                echo " value=" . $song["id"] . " id=request-button-" . $song["id"] ." ";
+                                echo " class=\"btn btn-material-red request-button btn-block\">Request</button></td>";
+                                echo "</tr>\n";
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> <!-- ./container-fluid -->
+        
+        <!-- jQuery 2.x JS -->
+        <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.1.3.js"><\/script>')</script>
+        
+        <!-- Bootstrap JS -->
+        <script src="js/vendor/bootstrap.min.js"></script>
+        
+        <!-- Material Design JS -->
+        <script src="js/vendor/material.min.js"></script>
+        <script src="js/vendor/ripples.min.js"></script>
+        <script src="js/vendor/snackbar.min.js"></script>
+        <script src="js/vendor/jquery.nouislider.all.min.js"></script>
+        
+        <!-- SortTable -->
+        <script src="js/vendor/sorttable.js"></script>
+
+        <!-- Main JS -->
+        <script src="js/request.js"></script>
+
+        <!-- Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-37431936-4', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </body>
+</html>
